b77c24bd9d23d3a26cd69ba3c49e17ca87586cce
Sooniii <theodulejanchen@hotmail.fr>
2021-03-03 16:43:39 +0100

    story 1, ajout dans le panier

--- js/app.js
+++ js/app.js
@@ -1,21 +1,33 @@
 const panierButton = document.querySelectorAll('.add-to-cart');
-const list = document.querySelector('#cart');
+const tBody = document.querySelector('tbody');
 let allArticle = [];
 
 
-function ajouterPanier() {
-    id = Date.now();
-    const item = document.createElement('li');
-    item.setAttribute('data-key', id);
-    const txt = document.createElement('span');
-    txt.innerText = 'test'
-    item.appendChild(txt);
-    list.appendChild(item);
-    allArticle.push(item);
-    console.log(allArticle);
+function ajouterPanier(event) {
+    if (event.target.parentElement.querySelector('.stock').innerHTML > 0) {
+        event.target.parentElement.querySelector('.stock').innerHTML -= 1;
+        let src = event.target.parentElement.parentElement.querySelector('img').getAttribute('src');
+        id = Date.now();
+        const img = document.createElement('img');
+        img.setAttribute('src', src);
+        img.width = 100;
+        const item = document.createElement('li');
+        item.setAttribute('data-key', id);
+        const txt = document.createElement('span');
+        let attrib = event.target.getAttribute('data-id');
+        txt.innerHTML = COURSES[attrib].title + " " + COURSES[attrib].price;
+        COURSES[attrib].stock -= 1;
+        item.appendChild(img);
+        item.appendChild(txt);
+        tBody.appendChild(item);
+        allArticle.push(item);
+    }
+}
+
+for(let i = 0; i < panierButton.length; i++) {
+    panierButton[i].addEventListener('click', ajouterPanier);
 }
 
 
-panierButton[0].addEventListener('click', ajouterPanier);
 
 
