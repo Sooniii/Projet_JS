aa5a9d50d85afe562f8f44be4b023a8c9db612b1
Loic-Gouriou <loicgouriou6@gmail.com>
2021-03-04 15:52:44 +0100

    notif

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
 const panierButton = document.querySelectorAll('.add-to-cart');
 const tBody = document.querySelector('tbody');
+const notif = document.querySelector('.header')
 let allArticle = [];
-
 
 function ajouterPanier(event) {
     if (event.target.parentElement.querySelector('.stock').innerHTML > 0) {
         event.target.parentElement.querySelector('.stock').innerHTML -= 1;
         let src = event.target.parentElement.parentElement.querySelector('img').getAttribute('src');
         id = Date.now();
+        const newDiv = document.createElement('div');
+        newDiv.innerHTML = "Cours ajouter au panier <br>lolo";
         const img = document.createElement('img');
         img.setAttribute('src', src);
         img.width = 100;
@@ -20,6 +22,12 @@ function ajouterPanier(event) {
         item.appendChild(img);
         item.appendChild(txt);
         tBody.appendChild(item);
+        notif.appendChild(newDiv);
+        setTimeout(function(){
+            console.log('en attente');
+            newDiv.innerHTML = "<br>";
+            notif.removeChild(newDiv);
+        }, 3000);
         allArticle.push(item);
     }
 }
@@ -27,7 +35,5 @@ function ajouterPanier(event) {
 for(let i = 0; i < panierButton.length; i++) {
     panierButton[i].addEventListener('click', ajouterPanier);
 }
-
-
 
 
