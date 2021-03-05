ff14f5e47b88b10bfff7df005c755ee1a0037c0a
Shadow <kcoudray2001@gmail.com>
2021-03-05 14:10:08 +0100

    Merge et ajout de la fonction supprimer article

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//Récuperation d'éléments html
 const panierButton = document.querySelectorAll('.add-to-cart');
 const viderPanierButton = document.querySelector('#empty-cart')
 const tBody = document.querySelector('tbody');
@@ -5,14 +6,18 @@ const notif = document.getElementById('notification_container');
 
 let totalPrice = 0;
 
+//Ajout d'event sur les boutons 
 for(let i = 0; i < panierButton.length; i++) {
     panierButton[i].addEventListener('click', ajouterPanier);
 }
 viderPanierButton.addEventListener('click', viderPanier);
 
+//Clear du storage quand on arrive sur le site
 window.localStorage.clear();
 
+//Fonction permettant d'ajouter un cours dans le panier
 function ajouterPanier(event) {
+    //Crée la balise qui affiche le prix total si aucun cours n'est dans le panier
     if (tBody.firstChild == null) {
         const priceLi = document.createElement('li');
         const priceTxt = document.createElement('span');
@@ -20,6 +25,7 @@ function ajouterPanier(event) {
         priceLi.appendChild(priceTxt);
         tBody.appendChild(priceLi);
     }
+    //Rajoute un article dans le panier si la quantité est disponible
     if (event.target.parentElement.querySelector('.stock').innerHTML > 0) {
         event.target.parentElement.querySelector('.stock').innerHTML -= 1;
         let src = event.target.parentElement.parentElement.querySelector('img').getAttribute('src');
@@ -35,18 +41,19 @@ function ajouterPanier(event) {
         let newDiv = document.createElement('div');
         newDiv.innerHTML = "<p>Cours : " + COURSES[attrib].title + " " + "Cours ajouter au panier</p>";
         newDiv.className = "content";
-        COURSES[attrib].stock -= 1;
+        let buttonSuppr = document.createElement('button');
+        buttonSuppr.innerHTML = 'supprimer';
         item.appendChild(img);
         item.appendChild(txt);
-        item.appendChild(buttonSuppr)
+        item.appendChild(buttonSuppr);
         notif.appendChild(newDiv);
         setTimeout(function(){
             notif.removeChild(newDiv);
         }, 3000);
         tBody.appendChild(item);
+        buttonSuppr.addEventListener('click', supprimerPanier);
         if (window.localStorage.getItem(COURSES[attrib].title) == null) {
             window.localStorage.setItem(COURSES[attrib].title, 1);
-        buttonSuppr.addEventListener('click', supprimerPanier);
         }else{
             let newQuantity = parseInt(window.localStorage.getItem(COURSES[attrib].title)) + 1;
             window.localStorage.setItem(COURSES[attrib].title, newQuantity);
@@ -59,8 +66,14 @@ function ajouterPanier(event) {
         newPrice.innerHTML = 'Prix total du panier : ' + totalPrice + '€'
     }
 }
+//Supprime les articles
+function supprimerPanier(event){
+    event.target.parentElement.remove();
+}
 
+//Vide la panier
 function viderPanier(e){
+    totalPrice = 0;
     window.localStorage.clear();
     while(tBody.firstChild ){
         tBody.removeChild(tBody.firstChild);
@@ -82,7 +95,3 @@ function viderPanier(e){
         notif.removeChild(newDiv);
     }, 3000);
 }
-
-
-
-
